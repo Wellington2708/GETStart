54e09a0502d9472455c656792cb12b083ba29364
Wellington Junior <wellingtonjunior1027@gmail.com>
2025-11-21 11:18:42 +0000

    learning variables

---
+++ src/variables/index.js
@@ -0,0 +1,22 @@
+// console.log("Digite o nome do seu jogador:");
+
+// let nickName = "Wellington";
+
+// console.log("Bem-vindo " + nickName);
+
+// const notificacao = "Pokemon Go diz: "
+
+// console.log(notificacao + "Tem um novo pokemon na região");
+// console.log(notificacao + "Você foi derrotado por um líder");
+
+console.log("Practice real world");
+
+// We have 3 jars
+
+let jarCoffee = "Café 3 Rancheiros";
+let jarSugar = "Açucar União";
+let jarCookie = "Biscoito de Trigo"; 
+
+console.log("Na cozinha tem " + jarCoffee + "," + jarCookie + "," + jarSugar);
+//or
+console.log(`Na cozinha tem ${jarCoffee}, ${jarCookie} e ${jarSugar}`);
---
+++ src/variables/types.js
@@ -0,0 +1,25 @@
+let namePokemon = "pikachu";
+let level = 20;
+let points = 45;
+//let gender = "male";
+let selectable = true; 
+
+// Strig armazena texto
+// number armazena números
+// boolenos são variaveis logicas que armazenam verdadeiro ou falso
+// Inteiros armazenam números que não quebrados
+// Ponto flutuante armazena numero com casas decimais
+// Caractere armazena só uma letra e são representadas por aspas simples
+// Date armazena data e hora
+
+
+// exercise real world
+
+let name = "Benedita";
+let age = 81;
+let numberPhone = "(55)9999999999";
+let socialNumber = "80808080808";
+let street = "C5 Avenue";
+let gender = 'F';
+let receivesPension = true;
+
