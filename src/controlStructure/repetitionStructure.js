6ad8ff3a8c3cc706cd7970b96af5fc571e29fdda
Wellington Junior <wellingtonjunior1027@gmail.com>
2025-11-23 21:17:10 +0000

    finished control structure

---
+++ src/controlStructure/repetitionStructure.js
@@ -0,0 +1,34 @@
+for(let count = 0; count < 4; count++){
+    //console.log(count);
+}
+
+
+// outro exemplo FOR
+
+let life = 0;
+
+for(i = 0; i <= 5; i++){
+    life += 1;
+    //console.log("Tomou porção mágica " + life);
+}
+
+    //console.log("Totais de vida: " + life );
+
+
+// While
+
+let countWhile = 1;
+
+while(countWhile < 11){
+    console.log("Essa mensagem tem número " + countWhile);
+    countWhile++
+}
+
+// DO While
+
+ let countDoWhile = 1;
+
+ do {
+    console.log("Essa mensagem é de número: " + countDoWhile);
+    countDoWhile++
+ } while (countDoWhile <= 3)
